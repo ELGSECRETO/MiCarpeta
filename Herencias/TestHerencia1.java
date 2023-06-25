d7a1e15af27a7d2eb7a2a6036ac9ac5b65d98f32
Emanuel Luna Garcia <elgsecreto@gmail.com>
2023-06-24 21:50:30 -0600

    Agrego primer apartado de herencias

---
+++ Herencias/TestHerencia1.java
@@ -0,0 +1,11 @@
+package MiCarpeta.Herencias;
+
+//Código de test aprenderaprogramar.com
+public class TestHerencia1 {
+
+    public static void main (String [ ] Args) {
+        Profesor profesor1 = new Profesor ("Juan", "Hernández García", 33);
+        profesor1.setIdProfesor("Prof 22-387-11");
+        profesor1.mostrarNombreApellidosYCarnet();}
+
+} //Cierre de la clase
