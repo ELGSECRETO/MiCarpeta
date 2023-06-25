cf1fe73d16fea113a44c3a6a4df81ded913c960e
ELunaGarcia <73920718+ELGSECRETO@users.noreply.github.com>
2023-06-24 22:14:16 -0600

    Modifico TestHerencia1 (#1)

--- Herencias/TestHerencia1.java
+++ Herencias/TestHerencia1.java
@@ -4,8 +4,8 @@ package MiCarpeta.Herencias;
 public class TestHerencia1 {
 
     public static void main (String [ ] Args) {
-        Profesor profesor1 = new Profesor ("Juan", "Hernández García", 33);
-        profesor1.setIdProfesor("Prof 22-387-11");
+        Profesor profesor1 = new Profesor ("Emanuel", "Luna García", 23);
+        profesor1.setIdProfesor("2771123");
         profesor1.mostrarNombreApellidosYCarnet();}
 
 } //Cierre de la clase
